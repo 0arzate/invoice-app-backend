737f7c4587545842a3486dfc54bae307ebad17a3
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2025-06-13 22:11:37 -0600

    feat(invoices): ✨ add amount field

--- app/controllers/invoices.controller.js
+++ app/controllers/invoices.controller.js
@@ -1,8 +1,25 @@
 import { INVOICES } from '../constants/invoices'
 
 export function getInvoicesController (req, res) {
+  const invoicesNormalized = INVOICES.map((invoice) => {
+    const items = invoice.items
+
+    const amount = items.reduce((total, item) => {
+      const amount = item.price
+      const itemsAmount = item.quantity * amount
+      const totalAmount = total + itemsAmount
+
+      return totalAmount
+    }, 0)
+
+    return {
+      ...invoice,
+      amount
+    }
+  })
+
   res.status(200).json({
-    data: INVOICES,
+    data: invoicesNormalized,
     totalItems: INVOICES.length
   })
 }
