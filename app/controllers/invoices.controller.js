eb661d5c9df7286bc2309fb714eee112cb8d5a1d
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2025-07-30 06:14:32 -0600

    feat(invoices): ✨ add query status

--- app/controllers/invoices.controller.js
+++ app/controllers/invoices.controller.js
@@ -1,6 +1,8 @@
 import { INVOICES } from '../constants/invoices.js'
 
 export function getInvoicesController (req, res) {
+  const { status } = req.query
+
   const invoicesNormalized = INVOICES.map((invoice) => {
     const items = invoice.items
 
@@ -17,6 +19,15 @@ export function getInvoicesController (req, res) {
       amount
     }
   })
+
+  if (status) {
+    const filteredInvoices = invoicesNormalized.filter(invoice => status.includes(invoice.status))
+
+    return res.status(200).json({
+      data: filteredInvoices,
+      totalItems: filteredInvoices.length
+    })
+  }
 
   res.status(200).json({
     data: invoicesNormalized,
